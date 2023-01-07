2ed90c8de79481f3def01aefdbbef02950eb2d8a
Raven-Vitos <ravenvitos@gmail.com>
2023-01-07 22:33:17 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+	include 'config.php';
+	
+	try 
+	{
+		$db = new PDO("mysql:host=localhost;dbname=$dbn", $user, $pass);
+		
+		if (!$db)
+		{
+			echo "Error!";			
+		}
+		else
+		{
+			foreach($db->query("SELECT * FROM t1") as $row) 
+			{
+				echo $row['id'] . " - " . $row['name'] . "<br>";
+			}
+		}
+	}
+	catch (PDOExection $e)
+	{
+		print($e->getMessage());
+	}	
+
+	echo "<h1>Hello World!</h1>";
+?>
